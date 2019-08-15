7547484609e808c05f9bbd5a35bb87a2d56808c9
John MacIntire <jrwmacintire@gmail.com>
2019-08-14 23:00:54 -0700

    All functional tests written. Debugger working.

--- routes/api.js
+++ routes/api.js
@@ -1,81 +1,79 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-const expect = require('chai').expect,
-      MongoClient = require('mongodb'),
-      mongoose = require('mongoose'),
-      os = require('os'),
-      networkInterfaces = os.networkInterfaces();
+const expect = require("chai").expect;
+const MongoClient = require("mongodb");
+const mongoose = require("mongoose");
+const os = require("os");
+const networkInterfaces = os.networkInterfaces();
 
 // Model
-const Stock = require('../models/Stock.js');
+const Stock = require("../models/Stock.js");
 
 // Controller
-const StockHandler = require('../controllers/stockHandler.js');
+const StockHandler = require("../controllers/stockHandler.js");
 const stockHandler = new StockHandler();
 
-const validateStock = require('../lib/validateStock.js');
+const validateStock = require("../lib/validateStock.js");
 
 const DB_URL = process.env.DB_URL;
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      // console.log(`req.query: `, req.query);
-      // res.send(req.query);
+  app.route("/api/stock-prices").get(function(req, res) {
+    // console.log(`req.query: `, req.query);
+    // res.send(req.query);
 
-      const ipAddress = networkInterfaces.lo[0].address;  
-      console.log(`IP address: ${ipAddress}`);
-    
+    try {
+      const ipAddress = req.host;
+      // console.log(`IP address: ${ipAddress}`);
+
       const stockName = req.query.stock || req.query.stock[0];
-  
-      // console.log(`Stock :`, stock);      
-  
+
+      const stock = new Stock({
+        stock: stockName.toUpperCase(),
+        price: "135.28",
+        likes: 1,
+        price_updated: { type: Date, default: new Date() },
+        ipAddresses: [ipAddress]
+      });
+      // console.log(`Stock :`, stock);
+
       mongoose.connect(DB_URL, { useNewUrlParser: true });
-  
+
       const db = mongoose.connection;
-  
+
       // On database connection error
-      db.on('error', console.error.bind(console, 'Connection error:'));
-  
+      db.on("error", console.error.bind(console, "Connection error:"));
+
       // Received 'open' event from database.
-      db.on('open', () => {
-        console.log('Inside the open database connection.');
+      db.on("open", () => {
+        console.log("Inside the open database connection.");
         // res.send(stock);
         // console.log(`stockHandler.updateDate: ${stockHandler.updateDate()}`);
-        const stock = new Stock({
-          stock: stockName.toUpperCase(),
-          price: "135.28",
-          likes: 1,
-          price_updated: { type: Date, default: new Date },
-          ipAddresses: [ipAddress]
-        });
         
-        stock.save((err, stock) => {
-          if(err) throw err;
-          res.send(stock);
-        });
-        
+        res.json(stock);
       });
-  
-      db.on('close', () => {
-        console.log('Disconnected from the DB!');
-      });
-    
-    });
-    
-    // const validStock = validateStock(stockName).then(res => { return res });
 
-    //   console.log(`is stock valid?: ${validStock}`);
-  
-    // });
-    
+      db.on("close", () => {
+        console.log("Disconnected from the DB!");
+        // res.send(stock);
+      });
+    } catch(err) {
+      throw err;
+    }
+  });
+
+  // const validStock = validateStock(stockName).then(res => { return res });
+
+  //   console.log(`is stock valid?: ${validStock}`);
+
+  // });
 };
